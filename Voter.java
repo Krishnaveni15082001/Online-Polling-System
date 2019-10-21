cbbe847b30c942227eecc6cb908b3b135faaa034
Krishna Veni Mekkina <kvmekkina@gmail.com>
2019-10-21 15:55:03 +0530

    add Voter

---
+++ Voter.java
@@ -0,0 +1,70 @@
+package first;
+
+public class Voter {
+	private int voterId;
+	private String voterName;
+	private int voterAge;
+	//private final int castedVote;
+	private int vote;
+	private String password;
+	
+	static int counter = 1000;
+
+	public Voter() {
+		super();
+	}	
+
+	public Voter(String voterName, int voterAge, String password) {
+		super();
+		this.voterId = counter++;
+		this.voterAge = voterAge;
+		this.voterName = voterName;
+		this.password = password;
+	}
+	
+	public int getVoterId() {
+		return voterId;
+	}
+
+	public String getVoterName() {
+		return voterName;
+	}
+
+	public void setVoterName(String voterName) {
+		this.voterName = voterName;
+	}
+	
+	public int getVote() {
+		return vote;
+	}
+
+	public void setVote(int vote) {
+		this.vote = vote;
+	}	
+	
+	public int getVoterAge() {
+		return voterAge;
+	}
+
+	public void setVoterAge(int voterAge) {
+		this.voterAge = voterAge;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public static int getCounter() {
+		return counter;
+	}
+
+	@Override
+	public String toString() {
+		return "Voter [Id=" + voterId + ", Name=" + voterName + ", Age=" + voterAge + "]";
+	}    
+	
+}
