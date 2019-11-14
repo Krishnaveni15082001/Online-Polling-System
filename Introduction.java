10345d222c2e1bb7c488c7f17751680832c18e91
Krishna Veni Mekkina <kvmekkina@gmail.com>
2019-11-14 12:48:32 +0000

    Update Introduction.java

--- Introduction.java
+++ Introduction.java
@@ -1,3 +1,5 @@
+package first;
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -13,50 +15,51 @@ import first.Functions.passwordStrength;
 
 public class Introduction {
 
-	
 	public static void main(String[] args) throws IOException, ClassNotFoundException {
-		
+
 		int ch, key = 0;
 		Functions functions = new Functions();
 		LoginVoter login = new LoginVoter();
 		Register register = new Register();
 		Scanner scan = new Scanner(System.in);
-		
+
 
 		do {
-			
+
 			functions.displayMainMenu();			
 			ch = scan.nextInt();
-			
+
 			switch(ch) {
-			
-				case 1:
-					login.login();
-					break;					
-					
-				case 2:					
-					functions.displayRegisterMenu();					
-					key = scan.nextInt();
-					
-					switch(key) {
-					
-						case 1:		register.voterRegister();
-									break;
-															
-						case 2:		
-									register.nomineeRegister();
-									break;
-							
-						case 3:		break;				
-					}	
-					
-				case 3:
-					break;
-				default:
-					System.out.println("Wrong selection made!");
+
+			case 1:
+				login.login();
+				break;					
+
+			case 2:					
+				functions.displayRegisterMenu();					
+				key = scan.nextInt();
+
+				switch(key) {
+
+				case 1:		register.voterRegister();
+				break;
+
+				case 2:		
+
+					break;		
+				}	
+				break;
+
+			case 3:
+				functions.adminLogin();
+				break;
+
+			case 4:
+				break;
+			default:
+				System.out.println("Wrong selection made!");
 			}
-		}while(ch !=3);
-		
-		
+		}while(ch != 4);
 	}
+
 }
